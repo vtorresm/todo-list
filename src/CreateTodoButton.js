cb4e4721eeede01c4a08341bbc57af2ea4f6a057
vtorresm <vatorresm88@gmail.com>
2022-09-27 17:43:49 -0500

    Agregando mis componentes para todo list

---
+++ src/CreateTodoButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './CreateTodoButton.css';
+
+function CreateTodoButton(props) {
+  return <button className='CreateTodoButton'>+</button>;
+}
+
+export { CreateTodoButton };
---
+++ src/TodoSearch.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './TodoSearch.css';
+
+function TodoSearch() {
+  return <input className='TodoSearch' placeholder='Cebolla' />;
+}
+export { TodoSearch };
