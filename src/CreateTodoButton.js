0e7561235cc3a5c51f92edc25d70aa1a55fd2703
vtorresm <vatorresm88@gmail.com>
2022-09-29 18:24:37 -0500

    manejando el estado de la aplicación

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,7 +2,18 @@ import React from 'react';
 import './CreateTodoButton.css';
 
 function CreateTodoButton(props) {
-  return <button className='CreateTodoButton'>+</button>;
+  const onClickButton = (msg) => {
+    alert(msg);
+  };
+
+  return (
+    <button
+      className='CreateTodoButton'
+      onClick={() => onClickButton('Aquí se debería abrir el modal')}
+    >
+      +
+    </button>
+  );
 }
 
 export { CreateTodoButton };
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
-  return <input className='TodoSearch' placeholder='Cebolla' />;
+
+  const [searchValue, setSearchValue] = useState('');
+  const onSearchValueChange = (event) => {
+    console.log(event.target.value);
+    setSearchValue(event.target.value);
+  };
+
+  return [
+    <input
+      className='TodoSearch'
+      placeholder='Cebolla'
+      value={searchValue}
+      onChange={onSearchValueChange}
+    />,
+    <p>{searchValue}</p>,
+  ];
 }
+
 export { TodoSearch };
