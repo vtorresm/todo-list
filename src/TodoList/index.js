c7f251ee0cfe159850c37725ae92170db8aa7a97
vtorresm <vatorresm88@gmail.com>
2022-10-06 17:45:17 -0500

    Terminado la organización de estructura

--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -8,4 +8,5 @@ function TodoList(props) {
     </section>
   );
 }
+
 export { TodoList };
--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -1,23 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './TodoSearch.css';
 
-function TodoSearch() {
-
-  const [searchValue, setSearchValue] = useState('');
+function TodoSearch({ searchValue, setSearchValue }) {
   const onSearchValueChange = (event) => {
     console.log(event.target.value);
     setSearchValue(event.target.value);
   };
 
-  return [
+  return (
     <input
       className='TodoSearch'
       placeholder='Cebolla'
       value={searchValue}
       onChange={onSearchValueChange}
-    />,
-    <p>{searchValue}</p>,
-  ];
+    />
+  );
 }
 
 export { TodoSearch };
